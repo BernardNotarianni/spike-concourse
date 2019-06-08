69c0f466e5d0b1e05a3accf99c449813ad13ec15
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-06-08 23:27:24 +0200

    gke deployment

--- my-webapp/my_webapp.py
+++ my-webapp/my_webapp.py
@@ -4,7 +4,7 @@ import my_domain
 @route('/')
 def index():
     message = my_domain.my_message()
-    return '<html><h1>My web app</h1><p>{}</p></html>'.format(message)
+    return '<html><h1>My web app redeployed</h1><p>{}</p></html>'.format(message)
 
 run(host='0.0.0.0', port=8000)
 
