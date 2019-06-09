babdc66bf3b2777d48dc018e2f033463e86c9355
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-06-09 19:22:36 +0200

    fix k8s template

--- deploy-tag.sh
+++ deploy-tag.sh
@@ -1,11 +1,12 @@
 TAG=$1
+NAMESPACE=$TAG
 FILENAME="k8s-$TAG.yml"
 
 echo "# Created by script $0" > $FILENAME
 echo "# From k8s_template.yml" >> $FILENAME
 echo "" >> $FILENAME
 
-cat k8s_template.yml | sed "s/___TAG___/$TAG/g" >> $FILENAME
+cat k8s_template.yml | sed "s/___TAG___/$TAG/g" | sed "s/___NAMESPACE___/$TAG/g" >> $FILENAME
 
 kubectl apply -f $FILENAME
 
