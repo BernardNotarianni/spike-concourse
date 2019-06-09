4d4a105ea5abd0b5563bd36de9c0b2d742608045
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-06-09 19:27:47 +0200

    fix app header

--- my-webapp/my_webapp.py
+++ my-webapp/my_webapp.py
@@ -4,7 +4,7 @@ import my_domain
 @route('/')
 def index():
     message = my_domain.my_message()
-    return '<html><h1>In Kubernetes dev-1</h1><p>{}</p></html>'.format(message)
+    return '<html><h1>Welcome to Kubernetes</h1><p>{}</p></html>'.format(message)
 
 run(host='0.0.0.0', port=8000)
 
