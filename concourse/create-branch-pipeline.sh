695578ecb4fff8f2130cc49f3645e192a6dc14f8
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-06-10 12:00:43 +0200

    create subdirs

--- concourse/create-branch-pipeline.sh
+++ concourse/create-branch-pipeline.sh
@@ -1,3 +1,6 @@
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 BRANCH=$(git symbolic-ref --short HEAD)
 
 TAG=$(echo $BRANCH| sed 's/\(dev-[0-9]*\).*/\1/')
@@ -10,8 +13,10 @@ echo "# Created by script $0" > $FILENAME
 echo "# From pipeline_template.yml" >> $FILENAME
 echo "" >> $FILENAME
 
-cat pipeline_template.yml | sed "s/___BRANCH___/$BRANCH/g" | sed "s/___TAG___/$TAG/g" >> $FILENAME
+TEMPLATE=$SCRIPTPATH/pipeline_template.yml
 
-fly -t tutorial set-pipeline -c $FILENAME -p $BRANCH -l credentials.yml
+cat $TEMPLATE | sed "s/___BRANCH___/$BRANCH/g" | sed "s/___TAG___/$TAG/g" >> $FILENAME
+
+fly -t tutorial set-pipeline -c $FILENAME -p $BRANCH -l ~/concourse-credentials.yml
 
 echo "Created concourse pipeline $TAG"
